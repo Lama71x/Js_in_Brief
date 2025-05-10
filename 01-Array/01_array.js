efe078c9d6519e7713cc264dbf69d7896f5305a4
Lama71x <webdev9849@gmail.com>
2025-05-10 17:38:20 +0000

    Array

---
+++ 01-Array/01_array.js
@@ -0,0 +1,28 @@
+//Array can be use to add multiple data in a single variable
+//There are Two ways to call the Array
+
+//Way 1
+
+// const mobileCompany = ["Samsung","Redmi","Readlmi","Apple"]
+
+// console.log(mobileCompany);
+
+// // Way to call Array
+// console.log(mobileCompany[0]); -->SAmsung
+// console.log(mobileCompany[1]); -->Redmi
+// console.log(mobileCompany[2]); -->Realmi
+// console.log(mobileCompany[3]); -->Apple
+
+
+//way 2 -->Using JavaScript new Array()
+
+// const mobileCompany = new Array("Samsung","Redmi","Readlmi","Apple")
+// console.log(mobileCompany);
+
+
+// console.log(mobileCompany[0]);
+// console.log(mobileCompany[1]);
+// console.log(mobileCompany[2]);
+// console.log(mobileCompany[3]);
+
+
